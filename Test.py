dede2b13238017953deed11ab6656044e4932d4d
M Sadiq Warsi <157520386+msw2005@users.noreply.github.com>
2026-02-04 11:49:48 +0000

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,2 +1,11 @@
 #Author: mohammad sadiq warsi
-def()
+
+def __init__(self, user_name: str):
+        """
+        Initialises the user object with an empty list of friend names.
+        :param user_name: name of the user.
+        """
+        self.name = user_name
+        self.friend_names = []
+
+"""def();"""
