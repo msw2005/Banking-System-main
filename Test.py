a6e167bc4b5a6268ab45106e7cec2b4d62fba3a5
M Sadiq Warsi <157520386+msw2005@users.noreply.github.com>
2026-04-21 16:27:41 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -4,8 +4,10 @@ def __init__(self, user_name: str):
         """
         Initialises the user object with an empty list of friend names.
         :param user_name: name of the user.
+        ..
         """
         self.name = user_name
+            
         self.friend_names = []
 
 """def();"""
